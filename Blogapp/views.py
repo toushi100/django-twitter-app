dea21a44019d1f4b43f0ad022a258523f31a2de1
toushi100 <toushi2009@gmail.com>
2020-09-11 02:29:46 +0200

    password reset and pagination

--- Blogapp/views.py
+++ Blogapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
@@ -8,6 +8,8 @@ from django.views.generic import (
     DeleteView
 )
 from .models import Post
+from django.contrib.auth.models import User
+
 from .models import Comment
 
 
@@ -17,11 +19,24 @@ def home (request):
         'posts': Post.objects.all()
     }
     return render(request , 'Blogapp/home.html', content)
+
 class PostListView(ListView):
     model  =  Post
     template_name = 'Blogapp/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
+
+class UserListView(ListView):
+    model  =  Post
+    template_name = 'Blogapp/user_post.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model  =  Post
--- blog/settings.py
+++ blog/settings.py
@@ -129,3 +129,10 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND ='django.core.email.mail.backend.smtp.EmailBacked'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
